b5cc402b8130798e4988236201c4224ef1a1a143
Adrian Wrona <adrianwrona96@gmail.com>
2023-01-26 22:03:23 +0100

    add test website

---
+++ object-page/website.page.ts
@@ -0,0 +1,10 @@
+import { expect, Locator, Page } from "@playwright/test";
+import { BasePage } from "./base.page";
+
+export class Website extends BasePage {
+	readonly page: Page;
+	constructor(page: Page) {
+		super(page);
+		this.page = page;
+	}
+}
